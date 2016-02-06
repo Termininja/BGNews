67e18fd58eb79e9faa924965790c6f50ca5e0980
Termininja <termininja@mail.bg>
2016-02-06 17:39:13 +0200

    showReply directive

---
+++ BGNews.Client/js/directives/links/showReply.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+newsApp.directive('showReply', function (LEVEL_OF_NESTED_COMMENTS) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            scope.showReplyLink = ++scope.commentLevel < LEVEL_OF_NESTED_COMMENTS;
+        }
+    }
+});
