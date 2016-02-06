2afa6413bd1ec42c3cb48333d55bf9b7135b46e9
Termininja <termininja@mail.bg>
2016-02-06 17:23:36 +0200

    postThumbnail.js and userThumbnail.js

---
+++ BGNews.Client/js/directives/postThumbnail.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+newsApp.directive('postThumbnail', function () {
+    return {
+        restrict: 'A',
+        templateUrl: '/templates/directives/post-thumbnail.html',
+        replace: false
+    }
+});
---
+++ BGNews.Client/js/directives/userThumbnail.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+newsApp.directive('userThumbnail', function () {
+    return {
+        restrict: 'A',
+        templateUrl: '/templates/directives/user-thumbnail.html',
+        replace: false
+    }
+});
