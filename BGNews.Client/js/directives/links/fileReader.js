312f6eadb4a5a454eabfb50f1ba05d8fb6d9f457
Termininja <termininja@mail.bg>
2016-02-06 02:54:25 +0200

    fileReader directive for input[type=file]

---
+++ BGNews.Client/js/directives/links/fileReader.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+newsApp.directive('fileReader', function ($q, $rootScope) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModel) {
+            element.bind('change', function (e) {
+                readFile().then(function (data) {
+                    ngModel.$setViewValue(data);
+                    $rootScope.profileImageUpdate();
+                });
+
+                function readFile() {
+                    var deferred = $q.defer();
+                    var reader = new FileReader()
+                    reader.onload = function (data) {
+                        deferred.resolve(data.target.result);
+                    }
+                    reader.readAsDataURL(e.target.files[0]);
+
+                    return deferred.promise;
+                }
+            });
+        }
+    };
+});
