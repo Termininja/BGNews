65930b02e1e56ca7fd6f568e48053445110d0429
Termininja <termininja@mail.bg>
2016-02-06 02:26:58 +0200

    SearchController

---
+++ BGNews.Client/js/controllers/SearchController.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+newsApp.controller('SearchController', function ($scope, $routeParams, postData, ITEMS_PER_PAGE) {
+    $scope.itemsPerPage = ITEMS_PER_PAGE;
+    $scope.isPost = true;
+    $scope.isUser = false;
+    $scope.currentPage = 1;
+    $scope.pages = 5;
+
+    $scope.getPage = function () {
+        var skip = ($scope.currentPage - 1) * ITEMS_PER_PAGE;
+        postData.search(skip, ITEMS_PER_PAGE, $routeParams.word).then(function (data) {
+            $scope.collection = data.posts;
+            $scope.totalItems = data.count;
+        });
+
+        $scope.isHome = true;
+    };
+
+    $scope.getPage();
+});
