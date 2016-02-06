cb052738bd86467d5c16ae622c11466c09e03435
Termininja <termininja@mail.bg>
2016-02-06 02:25:12 +0200

    TagsController

---
+++ BGNews.Client/js/controllers/TagsController.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+newsApp.controller('TagsController', function ($scope, $routeParams, $rootScope, postData, ITEMS_PER_PAGE) {
+    $rootScope.title = 'Tag ' + $routeParams.tag;
+    $scope.itemsPerPage = ITEMS_PER_PAGE;
+    $scope.isPost = true;
+    $scope.isUser = false;
+    $scope.currentPage = 1;
+    $scope.pages = 5;
+
+    $scope.getPage = function () {
+        var skip = ($scope.currentPage - 1) * ITEMS_PER_PAGE;
+        postData.getByTag(skip, ITEMS_PER_PAGE, $routeParams.tag).then(function (data) {
+            $scope.collection = data.posts;
+            $scope.totalItems = data.count;
+        });
+
+        $scope.isHome = true;
+    };
+
+    $scope.getPage();
+});
