d1de49599b253c71afb29f9a2aa38be662ce1fa4
Termininja <termininja@mail.bg>
2016-02-08 01:46:53 +0200

    new methods in PostController
    getTabPosts(), toggleTab()

--- BGNews.Client/js/controllers/PostController.js
+++ BGNews.Client/js/controllers/PostController.js
@@ -3,6 +3,7 @@
 
     newsApp.controller('PostController', function ($scope, $routeParams, $rootScope, postData) {
         $scope.replyToPost = true;
+        $scope.tabPostsShow = [true, false, false];
 
         $scope.getAllComments = function () {
             postData.getComments($routeParams.postId).then(function (commentsData) {
@@ -27,6 +28,7 @@
         $scope.addComment = function (newComment) {
             postData.addComment(newComment, $routeParams.postId, $scope.replyToCommentId).then(function (done) {
                 if (done) {
+                    $scope.getPost();
                     $scope.getAllComments();
                     $scope.replyToPost = true;
                     $scope.replyToCommentId = undefined;
@@ -43,14 +45,39 @@
             });
         };
 
-        postData.getPost($routeParams.postId).then(function (data) {
-            $rootScope.title = data.title;
-            $scope.post = data;
-            $scope.getAllComments();
+        $scope.getTabPosts = function () {
+            postData.getRecentPosts(5).then(function (recentPosts) {
+                $scope.recentPosts = recentPosts;
+                postData.getPopularPosts(4).then(function (popularPosts) {
+                    postData.getRandomPosts(4).then(function (randomPosts) {
+                        $scope.tabPosts = [recentPosts.slice(0, -1), popularPosts, randomPosts];
+                    });
+                });
+            });
+        };
 
-            //postData.getRelatedPosts($scope.post.tags, 5).then(function (data) {
-            //    console.log(data);
-            //});
-        });
+        $scope.toggleTab = function (tab) {
+            $scope.tabPostsShow = new Array(3);
+            $scope.tabPostsShow[tab] = true;
+        };
+
+        $scope.getPost = function () {
+            postData.getPost($routeParams.postId).then(function (data) {
+                $rootScope.title = data.title;
+                $scope.post = data;
+                $scope.getAllComments();
+
+                postData.getRecentComments(5).then(function (recentComments) {
+                    $scope.recentComments = recentComments;
+                });
+
+                postData.getRelatedPosts($routeParams.postId, $scope.post.tags, 5).then(function (relatedPosts) {
+                    $scope.relatedPosts = relatedPosts;
+                });
+            });
+        };
+
+        $scope.getPost();
+        $scope.getTabPosts();
     });
 }());
